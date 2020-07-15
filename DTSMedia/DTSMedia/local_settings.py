240c77cd9e1017134b7c88ed6ebd7c55cf417821
Cole <61484562+cdockter12@users.noreply.github.com>
2020-07-15 13:18:16 -0400

    Delete local_settings.py

--- DTSMedia/DTSMedia/local_settings.py
+++
@@ -1,4 +0,0 @@
-EMAIL_HOST_USER = "downthestreetmediaco@gmail.com"
-EMAIL_HOST_PASSWORD = "Ph@sez867"
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$a1=1hm5*p7w+92n1g_yvl6efnthuak#gylbn2gv5j@5d&8j)9'
